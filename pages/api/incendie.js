f237002e9994dd679324952119f5a7a6b009846b
paginjulien <julien.pagin.pv@gmail.com>
2025-06-03 08:14:19 +0200

    Update incendie.js

--- pages/api/incendie.js
+++ pages/api/incendie.js
@@ -1,6 +1,6 @@
 import Airtable from 'airtable';
 
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY! }).base(process.env.AIRTABLE_BASE_ID!);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
@@ -16,10 +16,10 @@ export default async function handler(req, res) {
     telephone: data.telephone,
     adresseBien: data.adresseBien,
     typeHabitation: data.typeHabitation,
-    Maison passive": data.maisonPassive ? "Oui" : "Non",
+    "Maison passive": data.maisonPassive ? "Oui" : "Non",
     descriptionHabitation: data.descriptionHabitation,
     facades: data.facades,
-    facades moins de 5 M: data.facadeMoins5m ? "Oui" : "Non",
+    "Facades moins de 5 M": data.facadeMoins5m ? "Oui" : "Non",
     estimationContenu: data.estimationContenu,
     Sauna: data.sauna ? "Oui" : "Non",
     Piscine: data.piscine ? "Oui" : "Non",
@@ -27,10 +27,10 @@ export default async function handler(req, res) {
     Chaufferie: data.chaufferie ? "Oui" : "Non",
     Bureau: data.bureau ? "Oui" : "Non",
     Véranda: data.veranda ? "Oui" : "Non",
-    Autre pièce": data.autrePiece ? "Oui" : "Non",
-    Salle de jeux": data.salleJeu ? "Oui" : "Non",
+    "Autre pièce": data.autrePiece ? "Oui" : "Non",
+    "Salle de jeux": data.salleJeu ? "Oui" : "Non",
     Cave: data.cave ? "Oui" : "Non",
-    Garage: data.garage, // Menu déroulant → OK en string
+    Garage: data.garage,
     Chambres: data.chambres,
   };
 
